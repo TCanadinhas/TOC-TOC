a56b04bbe74723873d42245215c45b722b8ceb94
Talita Canadinhas <talitacanadinhas@gmail.com>
2015-10-11 15:37:28 -0300

    Version 0.2
    * Scenes: Menu, Game, Tutorial.
    * Create a challenger spawner: don't work yet.

---
+++ Toc-Toc Game/Assets/Scripts/ChallengeBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChallengeBehaviour : MonoBehaviour {
+
+	public GameObject challenge, dog;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Instantiate(dog, this.transform.position, this.transform.rotation);
+	}
+}
