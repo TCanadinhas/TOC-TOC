cf24d1a699d1e16ecd37b3e3776e94e1fe774958
Talita Canadinhas <talitacanadinhas@gmail.com>
2015-10-12 20:28:47 -0300

    Some changes #1
    * menu
    * spawner script

--- Toc-Toc Game/Assets/Scripts/ChallengeBehaviour.cs
+++ Toc-Toc Game/Assets/Scripts/ChallengeBehaviour.cs
@@ -3,16 +3,34 @@ using System.Collections;
 
 public class ChallengeBehaviour : MonoBehaviour {
 
-	public GameObject challenge, dog;
+	public GameObject challenge, dog, cow;
+	private bool isInstantiated;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		isInstantiated = false;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		Instantiate(dog, this.transform.position, this.transform.rotation);
+		switch(Random.Range(0,2))
+		{
+			case 0:
+				challenge = dog;
+			break;
+
+			case 1:
+				challenge = cow;
+			break;
+		}
+
+		if(isInstantiated == false)
+		{
+			Instantiate(challenge, this.transform.position, this.transform.rotation);
+			isInstantiated = true;	
+		}
+
 	}
 }
